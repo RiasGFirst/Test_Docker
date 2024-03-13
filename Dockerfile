82ce3f4ca171bdecd36b6a5f5ba613cb55ebce76
RiasGFirst <gremoryrias69@proton.me>
2024-03-13 10:14:34 +0100

    v.0.0.4

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM node:12.18.3
 WORKDIR /app
 COPY . .
+RUN mkdir -p /app/config && cp config/.example.json /app/config/development.json
 RUN npm install
 EXPOSE 8080
 CMD ["npm", "start"]
--- index.js
+++ index.js
@@ -1,12 +1,35 @@
-// Build a simple express server
 const express = require('express');
+const path = require('path');
+const fs = require('fs');
 const app = express();
 const port = 8080;
 
+
+const configFilePath = path.resolve(__dirname, 'config/');
+console.log(configFilePath);
+let config;
+
 app.get('/', (req, res) => {
-    res.send(`Hello ${process.env.NAME || "Anonymous"}!`);
+    const configFilePath = path.resolve(__dirname, 'config/' + (process.env.NODE_ENV || 'development') + '.json');
+    let config;
+    try {
+        config = JSON.parse(fs.readFileSync(configFilePath, 'utf8'));
+    } catch (e) {
+        console.error('Error reading config file', e);
+    }
+
+    res.send(`Hello ${process.env.NAME || "Anonymous"} on ${config?.title || 'Default Title'}!`);
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`Test App listening at http://localhost:${port}`);
 });
+// app.get('/', (req, res) => {
+//     res.send(`Hello ${process.env.NAME || "Anonymous"}!`);
+//     const configFilePath = path.resolve(__dirname, '../config/' + (process.env.NODE_ENV || 'development') + '.json');
+//     let config;
+// });
+
+// app.listen(port, () => {
+//     console.log(`Example app listening at http://localhost:${port}`);
+// });
